7aee72b655683601116e4bf9f3332b566e4d19d2
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-23 01:34:24 +0530

    Update HandTracking.py

--- HandTracking.py
+++ HandTracking.py
@@ -11,6 +11,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+prevT = 0
+currT = 0
+
 
 
 
@@ -21,7 +24,22 @@ while True:
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks :
         for handLms in results.multi_hand_landmarks :
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+                if id==12:
+                    cv2.circle(img,(cx,cy),18,(255,0,255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img,handLms, mpHands.HAND_CONNECTIONS)
+
+    currT = time.time()
+    fps = 1/(currT-prevT)
+    prevT = currT
+
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_COMPLEX_SMALL,2,(0,0,0),2)
+
 
 
     cv2.imshow("Video", img)
